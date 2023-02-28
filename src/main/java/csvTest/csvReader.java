f0451a6cb2d92321ea14336d0956579f0632e999
Chris <cjm1n19@soton.ac.uk>
2023-02-28 11:42:19 +0000

    csv reader update

--- src/main/java/csvTest/csvReader.java
+++ src/main/java/csvTest/csvReader.java
@@ -6,14 +6,21 @@ import com.opencsv.*;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class csvReader {
-    private static final String IMPRESSION_LOG_FILEPATH = "/Users/chris/UniLocal/COMP2211/testData/2_month_campaign/impression_log.csv";
+    private static final String IMPRESSION_LOG_FILEPATH = "/Users/chris/UniLocal/COMP2211/testData/2_week_campaign_2/impression_log.csv";
 
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader(IMPRESSION_LOG_FILEPATH);
             CSVReader csvReader = new CSVReader(fileReader);
-            System.out.println(csvReader.readAll().size());
-//            List<Users> beans = new CsvToBeanBuilder<Users>(fileReader).withType(Users.class).build().parse();
+
+            String[] nextLine;
+
+            while ((nextLine = csvReader.readNext()) != null) {
+                for (String cell : nextLine) {
+                    System.out.print(cell + "\t");
+                }
+                break;
+            }
 
 //            System.out.println(beans.stream().filter(p -> p.getGender().equals("Male")).count());
 //            System.out.println(beans.get(0).toString());
