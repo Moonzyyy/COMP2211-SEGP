7f9dbed0fb5cee7983854aca2fcdc26eb1108722
dc4g21 <dc4g21@soton.ac.uk>
2023-03-01 14:05:46 +0000

    database version of app

--- src/main/java/csvTest/csvReader.java
+++ src/main/java/csvTest/csvReader.java
@@ -1,32 +1,68 @@
 package csvTest;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
-import java.util.List;
-import com.opencsv.*;
-import com.opencsv.bean.CsvToBeanBuilder;
+import java.sql.*;
 
 public class csvReader {
-    private static final String IMPRESSION_LOG_FILEPATH = "/Users/chris/UniLocal/COMP2211/testData/2_week_campaign_2/impression_log.csv";
+
+    private static final String IMPRESSION_LOG_FILEPATH = "C:\\Users\\danie\\OneDrive\\Desktop\\SEGCOURSEWORK\\week_campaign";
 
     public static void main(String[] args) {
-        try {
-            FileReader fileReader = new FileReader(IMPRESSION_LOG_FILEPATH);
-            CSVReader csvReader = new CSVReader(fileReader);
+        connector();
+    }
 
-            String[] nextLine;
-
-            while ((nextLine = csvReader.readNext()) != null) {
-                for (String cell : nextLine) {
-                    System.out.print(cell + "\t");
-                }
-                break;
+    public static void connector()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\danie\\OneDrive\\Desktop\\SEGCOURSEWORK\\src\\Database\\identifier.sqlite");
+            if(conn.isClosed())
+            {
+                System.out.println("Connection is closed");
+            }
+            else
+            {
+                System.out.println("Connection is open");
             }
 
-//            System.out.println(beans.stream().filter(p -> p.getGender().equals("Male")).count());
-//            System.out.println(beans.get(0).toString());
+            Statement stat = conn.createStatement();
+            stat.execute("Delete From myDatabase");
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO myDatabase VALUES(?,?,?,?,?,?,?)");
+            BufferedReader br = new BufferedReader(new FileReader(IMPRESSION_LOG_FILEPATH));
+            br.readLine();
+            final int batchSize = 10000;
+            int count = 0;
+            String line;
+            while ( (line=br.readLine()) != null)
+            {
+                String[] values = line.split(",");
+                statement.setString(1, values[0]);
+                statement.setString(2, values[1]);
+                statement.setString(3, values[2]);
+                statement.setString(4, values[3]);
+                statement.setString(5, values[4]);
+                statement.setString(6, values[5]);
+                statement.setString(7, values[6]);
+                statement.addBatch();
+                if (++count % batchSize == 0) {
+                    statement.executeBatch();
+                }
+            }
+            stat.close();
+            statement.executeBatch();
+            statement.close();
+            br.close();
+
+
         }
+        catch (Exception e)
+        {
+            System.out.println("Error = " + e.getMessage());
+        }
+
     }
 }
--- src/test/java/csvReaderTest.java
+++ src/test/java/csvReaderTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class csvReaderTest {
+import org.junit.jupiter.api.Test;
+
+
+
+public class csvReaderTest {
+  private static final String IMPRESSION_LOG_FILEPATH = "C:\\Users\\danie\\OneDrive\\Desktop\\SEGCOURSEWORK\\week_campaign\\impression_log.csv";
+
+  @Test
+  void runCSVReader()
+  {
+
+  }
+
 }
