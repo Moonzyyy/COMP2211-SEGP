75007d854ba9f3dc6f6a2c5d2de2d501a486802d
Isaac Day <16.iaday@gmail.com>
2023-03-02 14:43:03 +0000

    Added a title, and some buttons

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -2,44 +2,51 @@ package view;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 
-    //Start Menu
-    @Override
-    public void start(Stage stage) throws IOException {
-        //Simple start menu, no fxml
-        Button startButton = new Button("Import");
-        startButton.setOnAction(e -> {
-            System.out.println("Import button clicked");
-        });
-        Button settingsButton = new Button("Settings");
-        settingsButton.setOnAction(e -> {
-            System.out.println("Settings button clicked");
-        });
+  //Start Menu
+  @Override
+  public void start(Stage stage) throws IOException {
+    //Simple start menu, no fxml
+    Button startButton = new Button("Import");
+    startButton.setOnAction(e -> {
+      System.out.println("Import button clicked");
+    });
+    Button settingsButton = new Button("Settings");
+    settingsButton.setOnAction(e -> {
+      System.out.println("Settings button clicked");
+    });
 
-        var startBorderPane = new BorderPane();
+    var startBorderPane = new BorderPane();
 
-        var startButtonsVBox = new VBox();
-        startButtonsVBox.getChildren().addAll(startButton, settingsButton);
-        startBorderPane.setCenter(startButtonsVBox);
+    var startTitle = new VBox();
+    startTitle.setAlignment(Pos.CENTER);
+    startTitle.getChildren().add(new Label("AdViz"));
+    startBorderPane.setTop(startTitle);
 
-        var scene = new Scene(startBorderPane, 300, 300);
+    var startButtonsVBox = new VBox();
+    startButtonsVBox.getChildren().addAll(startButton, settingsButton);
+    startBorderPane.setCenter(startButtonsVBox);
+    startButtonsVBox.setSpacing(20);
+    startButtonsVBox.setAlignment(Pos.CENTER);
 
-        stage.setScene(scene);
-        stage.setTitle("AdViz");
-        stage.show();
+    var scene = new Scene(startBorderPane, 300, 300);
 
-        //Opens the start menu
-    }
+    stage.setScene(scene);
+    stage.setTitle("AdViz");
+    stage.show();
+  }
 
 }
