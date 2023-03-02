3f38162530384377b5fedc44e812a199a3c3d415
Isaac Day <16.iaday@gmail.com>
2023-03-02 13:55:09 +0000

    Rewrote it all without fxml. Need to do styling next

---
+++ src/main/java/com/adviz/Main.java
@@ -0,0 +1,45 @@
+package com.adviz;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public static void main(String[] args) {
+    launch();
+  }
+
+  //Start Menu
+  @Override
+  public void start(Stage stage) throws IOException {
+    //Simple start menu, no fxml
+    Button startButton = new Button("Import");
+    startButton.setOnAction(e -> {
+      System.out.println("Import button clicked");
+    });
+    Button settingsButton = new Button("Settings");
+    settingsButton.setOnAction(e -> {
+      System.out.println("Settings button clicked");
+    });
+
+    var startBorderPane = new BorderPane();
+
+    var startButtonsVBox = new VBox();
+    startButtonsVBox.getChildren().addAll(startButton, settingsButton);
+    startBorderPane.setCenter(startButtonsVBox);
+
+    var scene = new Scene(startBorderPane, 300, 300);
+
+    stage.setScene(scene);
+    stage.setTitle("AdViz");
+    stage.show();
+
+    //Opens the start menu
+  }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.example.group30 {
+module com.adviz {
   requires javafx.controls;
   requires javafx.fxml;
   requires com.opencsv;
   requires java.sql;
 
-  opens com.example.group30 to javafx.fxml;
-  exports com.example.group30;
+  opens com.adviz to javafx.fxml;
+  exports com.adviz;
   exports csvTest;
 }
