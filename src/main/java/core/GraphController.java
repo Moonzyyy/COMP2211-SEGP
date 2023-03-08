bcaf7f991dc5ef1912205bcd2eeedefbc9db7c9f
at16g21 <at16g21@soton.ac.uk>
2023-03-08 13:17:07 +0000

    Made a GraphController

---
+++ src/main/java/core/GraphController.java
@@ -0,0 +1,10 @@
+package core;
+
+import view.scenes.Graph;
+
+public class GraphController {
+    private final Graph theView;
+    public GraphController (Graph theView) {
+        this.theView = theView;
+    }
+}
