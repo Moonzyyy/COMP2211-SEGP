4c6827efa4bd1cb40c990ac6a5006c0a2d285c76
Isaac Day <16.iaday@gmail.com>
2023-03-12 22:20:16 +0000

    Pulled integration into frontend, now working on right branch

--- src/main/java/core/Controller.java
+++ src/main/java/core/Controller.java
@@ -212,7 +212,6 @@ public class Controller {
       Task<Void> task = new Task<>() {
         @Override
         public Void call() {
-          //TODO: Change the model so that this works
           model.importData();
           return null;
         }
