1177851e94c123c47840d73b08c810074574d652
Chris <cjm1n19@soton.ac.uk>
2023-03-12 14:43:42 +0000

    bd stuff

--- src/main/java/core/AdViz.java
+++ src/main/java/core/AdViz.java
@@ -5,7 +5,15 @@ import javafx.stage.Stage;
 import model.Model;
 import view.scenes.StartMenu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static model.CsvReader.distinctByKey;
 
 /**
  * The main class of the application.
@@ -29,8 +37,9 @@ public class AdViz extends Application {
     public static void main(String[] args) {
 
         try (Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:testDb", "sa", "");
-             Statement stmt = conn.createStatement();
         ){
+            conn.setAutoCommit(false);
+            Statement stmt = conn.createStatement();
 
             String agesTbl = "CREATE TABLE ages (id TINYINT IDENTITY PRIMARY KEY, label VARCHAR(5))";
             String incomesTbl = "CREATE TABLE incomes (id TINYINT IDENTITY PRIMARY KEY, label VARCHAR(6))";
@@ -49,13 +58,78 @@ public class AdViz extends Application {
             stmt.execute("INSERT INTO ages (label) VALUES ('<25'), ('25-34'), ('35-44'), ('45-54'), ('>54')");
             stmt.execute("INSERT INTO incomes (label) VALUES ('Low'), ('Medium'), ('High')");
             stmt.execute("INSERT INTO contexts (label) VALUES ('News'), ('Shopping'), ('Social Media'), ('Blog'), ('Hobbies'), ('Travel')");
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ages");
-            while(rs.next()) {
-                System.out.print(rs.getInt("id"));
-                System.out.println(rs.getString("label"));
+
+            InputStream impression_log = AdViz.class.getResourceAsStream("/testData/impression_log.csv");
+
+            PreparedStatement users = conn.prepareStatement("INSERT INTO users VALUES (?, ?, ?, ?, ?)");
+            BufferedReader br = new BufferedReader(new InputStreamReader(impression_log));
+            br.readLine();
+            final int batchSize = 50000;
+            ArrayList<Long> existing = new ArrayList<Long>();
+            int count = 0;
+            String line;
+
+//            Stream<String[]> uniques = br.lines().skip(1).map(line -> line.split(",")).filter(distinctByKey(x -> x[1]));
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                int age = 1;
+                switch (values[3]) {
+                    case "<25" -> age = 0;
+                    case "25-34" -> age = 1;
+                    case "35-44" -> age = 2;
+                    case "45-54" -> age = 3;
+                    case ">54" -> age = 4;
+                }
+
+                int income = 1;
+                switch (values[4]) {
+                    case "Low" -> income = 0;
+                    case "Medium" -> income = 1;
+                    case "High" -> income = 2;
+                }
+
+                int context = 1;
+                switch (values[5]) {
+                    case "News" -> context = 0;
+                    case "Shopping" -> context = 1;
+                    case "Social Media" -> context = 2;
+                    case "Blog" -> context = 3;
+                    case "Hobbies" -> context = 4;
+                    case "Travel" -> context = 5;
+                }
+                long id = Long.parseLong(values[1]);
+                if (!existing.contains(id)) {
+                    existing.add(id);
+                    users.setLong(1, id);
+                    users.setBoolean(2, values[2].equals("male"));
+                    users.setInt(3, age);
+                    users.setInt(4, income);
+                    users.setInt(5, context);
+                    users.addBatch();
+                    count++;
+                    if (count % batchSize == 0) {
+                        users.executeBatch();
+                        count = 0;
+                        System.out.println(existing.size());
+                    }
+                }
+
             }
+            System.out.println(existing);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            while (rs.next()) {
+                System.out.println(rs.getLong("id"));
+            }
+            stmt.close();
+            users.close();
+            conn.close();
+            System.out.println("DONE!!!!");
+
         }  catch (SQLException e) {
             e.printStackTrace(System.out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 //        AdViz app = new AdViz();
 //        launch();
