0ccd1dfcebece227f1a22912462772dfef61d18e
Isaac Day <16.iaday@gmail.com>
2023-03-17 17:47:38 +0000

    Fixed some stuff

--- src/main/java/view/scenes/Graph.java
+++ src/main/java/view/scenes/Graph.java
@@ -14,6 +14,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
