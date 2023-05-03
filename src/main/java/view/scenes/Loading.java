d1833fe8fb2754e648d3825c85c4f97dc2c97485
at16g21 <at16g21@soton.ac.uk>
2023-05-03 17:53:43 +0100

    Simple Loading Animation

--- src/main/java/view/scenes/Loading.java
+++ src/main/java/view/scenes/Loading.java
@@ -3,6 +3,9 @@ package view.scenes;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.concurrent.Task;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class Loading extends AbstractScene {
 
@@ -32,6 +36,23 @@ public class Loading extends AbstractScene {
         scene = new Scene(loadingPane, 1280, 720);
         scene.getStylesheets().add(
                 Objects.requireNonNull(getClass().getResource("/view/start.css")).toExternalForm());
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0), event -> loadingLabel.setText("Loading")),
+                new KeyFrame(Duration.seconds(0.5), event -> loadingLabel.setText("Loading.")),
+                new KeyFrame(Duration.seconds(1), event -> loadingLabel.setText("Loading..")),
+                new KeyFrame(Duration.seconds(1.5), event -> loadingLabel.setText("Loading...")),
+                new KeyFrame(Duration.seconds(2), event -> loadingLabel.setText("Loading"))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), loadingLabel);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.3);
+        fadeTransition.setAutoReverse(true);
+        fadeTransition.setCycleCount(FadeTransition.INDEFINITE);
+        fadeTransition.play();
     }
 
     public void setStyles(boolean theme) {
